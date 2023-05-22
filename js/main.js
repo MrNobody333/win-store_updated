70b8d58ad8c36cd611fc3c350414d0e5c8375b93
Nick <nick.khachaturian@gmail.com>
2023-05-22 13:38:55 +0300

    Finished the slider prototype

--- js/main.js
+++ js/main.js
@@ -5,3 +5,49 @@ navbarItem.addEventListener('click', () => {
   burgerMenu.firstChild.classList.toggle('span-active');
   burgerMenu.classList.toggle('burger-active');
 });
+
+const sliderArrow = document.querySelectorAll('.slider__arrow');
+const sliderImage = document.querySelectorAll('.slider__inner');
+let count = 0;
+
+sliderArrow.forEach(el => {
+  el.addEventListener('click', () => {
+    if (el.classList.contains('slider__arrow_next')) {
+      sliderImage.forEach(sl => {
+        sl.style.transform += 'translateX(-900px)';
+      });
+      count++;
+      if (count === 2) {
+        sliderImage.forEach(sl => {
+          sl.style.transform += 'translateX(300px)';
+        });
+      }
+      if (count === 3) {
+        sliderImage.forEach(sl => {
+          sl.style.transform += 'translateX(2400px)';
+        });
+        count = 0;
+      }
+    } else {
+      if (count === 0) {
+        sliderImage.forEach(sl => {
+          sl.style.transform = 'translateX(-1500px)';
+          count = 2;
+        });
+      } else if (count === 1) {
+        sliderImage.forEach(sl => {
+          sl.style.transform = 'translateX(0)';
+          count = 0;
+        });
+      } else if (count === 2) {
+        sliderImage.forEach(sl => {
+          sl.style.transform = 'translateX(-900px)';
+          count = 1;
+        });
+      }
+    }
+  });
+});
+
+
+
